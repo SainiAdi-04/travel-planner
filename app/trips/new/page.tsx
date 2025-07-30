fb68627f9e08e3621b8c244025f2d65090829b5c
ad <sainiadi03@gmail.com>
2025-07-30 19:50:37 +0530

    init

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader } from "@/components/ui/cards"
+
+export default function NewTrip() {
+    return (
+        <div className="max-w-lg mx-auto mt-10">
+            <Card>
+                <CardHeader>New Trip</CardHeader>
+                <CardContent>
+                    <form className="space-y-4">
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">{" "}Title</label>
+                            <input type="text" name="title" placeholder="Japan trip...." className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">Description</label>
+                            <textarea name="description" placeholder="Trip description..." className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-4">
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700">Start Date</label>
+                                <input type="date" name="startDate" className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                            </div>
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700">End Date</label>
+                                <input type="date" name="endDate" className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                            </div>
+                        </div>
+
+                        <Button type="submit" className="w-full">
+                            Create Trip
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
+
+        </div>
+    )
+}
