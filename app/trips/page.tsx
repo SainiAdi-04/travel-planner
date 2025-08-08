047a85e9f89f48ecb2eaa1aeb43b0efc661df3fc
ad <sainiadi03@gmail.com>
2025-08-09 00:28:45 +0530

    some features

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,9 +1,16 @@
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/cards";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
 export default async function TripsPage(){
-    const session =await auth;
+    const session =await auth();
+    const trips=await prisma.trip.findMany({
+        where:{userId:session?.user?.id}
+    })
+
+    const sortedTrips=[...trips].sort((a,b)=>new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
 
     if(!session){
         return <div className="flex justify-center items-center h-screen text-gray-700 text-xl">
@@ -12,14 +19,29 @@ export default async function TripsPage(){
         </div>
     }
 
+    const today= new Date();
+    today.setHours(0, 0, 0, 0);
+    const upcomingTrips = sortedTrips.filter(trip => new Date(trip.startDate) >= today);
+
     return <div className="space-y-6 container mx-auto px-4 py-8">
-        <div>
-            {" "}
-        <h1>Dashboard</h1>
+        <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
         <Link href={"/trips/new"}>
         <Button >New Trip</Button>
         </Link>
         </div>
+
+        <Card>
+            <CardHeader>
+                <CardTitle>Welcome Back, {session.user?.name}</CardTitle>
+            </CardHeader>
+
+            <CardContent>
+                <p>
+                    {trips.length===0?"Start palnning your first trip by clicking on the button above.":`You have ${trips.length} ${trips.length > 1 ? "trips" : "trip"} planned.`}
+                </p>
+            </CardContent>
+        </Card>
 
     </div>
 }
