bb2d27533bc08c70e25b5e95d7c8ddd2cadd943c
ad <sainiadi03@gmail.com>
2025-08-08 23:46:59 +0530

    added image uploader

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -3,8 +3,12 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/cards"
 import { createTrip } from "@/lib/actions/create-trip"
+import { UploadButton } from "@/lib/upload-thing"
+import Image from "next/image"
+import { useState } from "react"
 
 export default function NewTrip() {
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
     return (
         <div className="max-w-lg mx-auto mt-10">
             <Card>
@@ -30,6 +34,28 @@ export default function NewTrip() {
                                 <label className="block text-sm font-medium text-gray-700">End Date</label>
                                 <input type="date" name="endDate" className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
                             </div>
+                        </div>
+
+                        <div>
+                         <label>Trip Image</label>
+                         {imageUrl && (
+                            <Image src={imageUrl} alt="Trip Preview" className="w-full mb-4 rounded-md max-h-48 object-cover" 
+                            width={300}
+                            height={100}
+                            />
+                         )}   
+                         <UploadButton 
+                         endpoint='imageUploader' 
+                         onClientUploadComplete={(res)=>{
+                            if(res && res[0].ufsUrl){
+                                setImageUrl(res[0].ufsUrl);
+                            }
+                         }}
+
+                         onUploadError={(error: Error) => {
+                            console.error("Upload failed:", error) 
+                         }}  
+                         />
                         </div>
 
                         <Button type="submit" className="w-full">
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns: [{
+      hostname:"8epsdqx73b.ufs.sh"
+    },]
+  }
 };
 
 export default nextConfig;
